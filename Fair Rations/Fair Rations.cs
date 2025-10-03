b06b1df878338fa220e0dd9a5bad615de8492e57
Lhoredel <lhoredeldollentas@gmail.com>
2025-10-03 14:23:13 +0800

    Update Fair Rations.cs

--- Fair Rations/Fair Rations.cs
+++ Fair Rations/Fair Rations.cs
@@ -1,107 +1,72 @@
-#include <bits/stdc++.h>
+ using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-using namespace std;
+class Result
+{
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
+    /*
+     * Complete the 'fairRations' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY B as parameter.
+     */
 
-/*
- * Complete the 'fairRations' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts INTEGER_ARRAY B as parameter.
- */
-
-string fairRations(vector<int> B) {
-    int count = 0;
-    int n = B.size();
-    
-    for (int i = 0; i < n - 1; i++) {
-        if (B[i] % 2 != 0) {
+    public static string fairRations(List<int> B)
+    {
+         int count = 0;
+         int n = B.Count;
+         
+         for (int i = 0; i < n - 1; i++)
+         {
+        if (B[i] % 2 != 0)
+        {
            
             B[i]++;
             B[i + 1]++;
             count += 2;
         }
     }
-    
-    
-    if (B[n - 1] % 2 == 0) {
-        return to_string(count);
-    } else {
+
+    if (B[n - 1] % 2 == 0)
+    {
+        return count.ToString();
+    }
+    else
+    {
         return "NO";
     }
 }
 
+    }
 
 
-int main()
+
+class Solution
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-    string N_temp;
-    getline(cin, N_temp);
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
 
-    int N = stoi(ltrim(rtrim(N_temp)));
+        List<int> B = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(BTemp => Convert.ToInt32(BTemp)).ToList();
 
-    string B_temp_temp;
-    getline(cin, B_temp_temp);
+        string result = Result.fairRations(B);
 
-    vector<string> B_temp = split(rtrim(B_temp_temp));
+        textWriter.WriteLine(result);
 
-    vector<int> B(N);
-
-    for (int i = 0; i < N; i++) {
-        int B_item = stoi(B_temp[i]);
-
-        B[i] = B_item;
+        textWriter.Flush();
+        textWriter.Close();
     }
-
-    string result = fairRations(B);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
